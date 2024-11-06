0b920fb0a6dc6c4c0ce02697462dfbcb41530150
Lucas Macedo <lucas@autoforce.com>
2024-11-06 15:52:01 -0300

    chore: config eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.browser }},
+  {rules: {
+    eqeqeq: 'off',
+  }},
+  {ignores: ['./node_modules/']},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
