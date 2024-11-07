8c8bb618a2e5d2c1e82c6da985b9bc844133aed2
Lucas Macedo <lucas@autoforce.com>
2024-11-07 14:02:56 -0300

    chore: add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import 'dotenv/config';
+
 import './data-collect/queues/my-queue';
 import './data-collect/workers/my-worker';
-import './data-collect/app'
+import './data-collect/app';
 
 console.log('Aplicação iniciada com BullMQ e Redis');
