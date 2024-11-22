9337b56dad7a1b69d02087872c33fe7a7f11dbad
Lucas Macedo <lucas@autoforce.com>
2024-11-22 15:24:23 -0300

    feat: create a generic job factory

---
+++ src/usecases/collect-channels-performance-metrics/collect-channels-metrics.ts
@@ -0,0 +1,33 @@
+import { JobsOptions } from "bullmq";
+import myQueue from "../../infra/queue/queues/my-queue";
+import { Channel } from "../../data/types/channel";
+
+const defaultConfig: JobsOptions = {
+  removeOnComplete: {
+    age: 3600,
+    count: 1000,
+  },
+  removeOnFail: {
+    age: 24 * 3600
+  }
+}
+
+export async function addCollectChannelsPerformanceMetricsJobsToQueue(channelsList: Channel[], customJobConfig: JobsOptions = {}) {
+  if (channelsList?.length === 0) {
+    console.log('We have no channels to see metrics.')
+    return
+  }
+
+  channelsList.forEach(async (channel: Channel) => {
+    await myQueue.add(
+      'collectChannelPerformanceMetric',
+      {
+        channelUrl: channel.internal_link,
+        channelTheme: channel.theme
+      },
+      { ...defaultConfig, ...customJobConfig}
+    )
+  })
+  
+  console.log(`Collect metrics from ${channelsList?.length} channels.`)
+}
