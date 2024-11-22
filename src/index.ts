6ea384e9048a6d4d41c7adeed6682d7b3336a6e2
Lucas Macedo <lucas@autoforce.com>
2024-11-22 10:49:06 -0300

    refactor: using new functions to start application

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import 'dotenv/config';
+import 'reflect-metadata';
+
+import { connection } from './infra/db/connection';
 
 import './infra/queue/queues/my-queue';
 import './infra/queue/workers/my-worker';
 import './usecases/collect-channels-performance-metrics/index';
 
-import './infra/db/connection';
-import './data/models';
+import { addCollectChannelsPerformanceMetricsJobsToQueue } from './usecases/collect-channels-performance-metrics/index';
 
-console.log('Application is running at port 3333! 🚀');
+connection()
+  .then(() => {
+    console.log('Application is running at port 3333! 🚀');
+    addCollectChannelsPerformanceMetricsJobsToQueue();
+  })
+  
