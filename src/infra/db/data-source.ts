5e06e60ad4ac90cb914c71c673dd758affe0d706
Lucas Macedo <lucas@autoforce.com>
2025-01-03 17:54:39 -0300

    fix: set correct dist folder

--- src/infra/db/data-source.ts
+++ src/infra/db/data-source.ts
@@ -2,6 +2,8 @@ import 'dotenv/config';
 import { DataSource } from 'typeorm';
 import { env } from '../../config/env';
 
+const rootFolder = env.NODE_ENV === 'development' ? 'src' : 'dist'
+
 export const dataSource = new DataSource({
   type: 'postgres',
   host: env.TIMESCALEDB_HOST,
@@ -12,9 +14,9 @@ export const dataSource = new DataSource({
   synchronize: false,
   logging: true,
   entities: [
-    'src/domain/entities/*.{js,ts}'
+    `${rootFolder}/domain/entities/*.{js,ts}`
   ],
   migrations: [
-    'src/infra/db/migration/*.{js,ts}'
+    `${rootFolder}/infra/db/migration/*.{js,ts}`
   ]
 });
